ee4e7a8a6d886cad93de22bf796d2e6871620f28
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-10-18 01:40:07 +0530

    runapp modified

--- Frontend/ProjectStudentManagement/runapp.sh
+++ Frontend/ProjectStudentManagement/runapp.sh
@@ -1,15 +1,19 @@
 #! /usr/bin/bash 
 # sudo apt update 
 # sudo apt install python-pip 
-d="home"
+home="home"
+student="student"
 my_app_path=$( dirname $(readlink -f "$0"))
 echo $my_app_path
 source "$my_app_path/../pyenv/bin/activate"
-uiforms=($( ls $d | grep "\.ui" ))
+uiforms=($( ls $home | grep "\.ui" ))
 
+uiforms2=($( ls $student | grep "\.ui" ))
 
 for i in ${uiforms[@]}; do
-    pyside6-uic "$d/$i" -o "$d/${i::-3}.py"
+    pyside6-uic "$home/$i" -o "$home/${i::-3}.py"
 done
-
+for i in ${uiforms2[@]}; do
+    pyside6-uic "$student/$i" -o "$student/${i::-3}.py"
+done
 python "$my_app_path/main.py"
